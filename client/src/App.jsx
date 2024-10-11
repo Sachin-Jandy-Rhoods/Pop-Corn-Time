9e686c82d03471ce7eafb3373cc8bfa402ef4ba8
rufus005 <rufussam.orion@gmail.com>
2024-10-12 00:58:27 +0530

    Contact Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,16 @@ import { Route, Routes } from 'react-router-dom'
 import HomeScreen from './Screens/HomeScreen'
 import AboutUs from './Screens/AboutUs'
 import NotFound from './Screens/NotFound'
+import ContactUs from "./Screens/ContactUs";
+import Login from './Screens/Login'
 
 const App = () => {
   return (
     <Routes>
         <Route path='/' element={<HomeScreen/>}/>
         <Route path='/about-us' element={<AboutUs/>}/>
+        <Route path='/contact-us' element={<ContactUs/>}/>
+        <Route path='/login' element={<Login/>}/>
         <Route path='*' element={<NotFound/>}/>
     </Routes>
   )
