1793bb3688ef2a29587e81b5329bdd9689b5bd60
sachin-jandy-rhoods <sachinjandyrhoods@gmail.com>
2024-10-11 13:24:46 +0530

    Completed Untill Banner

---
+++ client/tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      // height
+      height:{
+        header:'560px',
+        rate:'400px',
+      },
+
+      // Font size
+      fontSize: {
+        h1: '2.6rem',
+      },
+
+      // Screen size
+      screens: {
+        xs: '475px',
+      },
+
+      // Colors
+      colors: {
+        main: '#080A1A',
+        subMain: '#F20000',
+        dry: '#0B0F29',
+        star: '#FFB000',
+        text: '#C0C0C0',
+        border: '#4b5563',
+        dryGray: '#E0D5D5',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
+}
