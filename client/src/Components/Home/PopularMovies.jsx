793cbe3ff6a9af645e39e77e04ec346e29c70a74
sachin-jandy-rhoods <sachinjandyrhoods@gmail.com>
2024-10-11 15:07:05 +0530

    change Admin folder t]into dashboard

--- client/src/Components/Home/PopularMovies.jsx
+++ client/src/Components/Home/PopularMovies.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Titles from '../Titles'
+import {BsCollectionFill} from "react-icons/bs"
 
 const PopularMovies = () => {
   return (
-    <div>PopularMovies</div>
-  )
+    <div className="my-16">
+        <Titles title="Popular Movies" Icon={BsCollectionFill}/>
+    </div>
+  ) 
 }
 
 export default PopularMovies
