9d49ee43c71e05d4845e738879544d14458d35f1
Rohith-Dhayanidhi <rohithdhayanidhi@gmail.com>
2024-10-18 00:30:30 +0530

    Delete Profile Integration for User

--- client/src/Redux/APIs/userServices.jsx
+++ client/src/Redux/APIs/userServices.jsx
@@ -1,39 +1,52 @@
 import Axios from "./Axios.jsx";
 
 //register new user API call
-const registerService=async(user)=>{
-    const {data}=await Axios.post("/users",user);
-    if(data){
-        localStorage.setItem("userInfo",JSON.stringify(data));
-    }
-    return data;
-}
-
-//logout user Function
-const logoutService=()=>{
-    localStorage.removeItem("userInfo");
-    return null  
-}
-
-//login user
-const loginServices=async(user)=>{
-    const {data}=await Axios.post("/users/login",user);
-    if(data){
-        localStorage.setItem("userInfo",JSON.stringify(data));
-    }
-    return data;
-}
-
-const updateProfileService = async (user, token) => {
-    const { data } = await Axios.put("/users", user, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        }
-    });
-    if (data) {
-        localStorage.setItem("userInfo", JSON.stringify(data));
-    }
-    return data;
+const registerService = async (user) => {
+  const { data } = await Axios.post("/users", user);
+  if (data) {
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  }
+  return data;
 };
 
-export {registerService,logoutService,loginServices,updateProfileService}
+//logout user Function
+const logoutService = () => {
+  localStorage.removeItem("userInfo");
+  return null;
+};
+
+//login user
+const loginServices = async (user) => {
+  const { data } = await Axios.post("/users/login", user);
+  if (data) {
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  }
+  return data;
+};
+
+const updateProfileService = async (user, token) => {
+  const { data } = await Axios.put("/users", user, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (data) {
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  }
+  return data;
+};
+
+// delete profile API call  
+const deleteProfileService = async (token) => {
+  const { data } = await Axios.delete("/users", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (data) {
+    localStorage.removeItem("userInfo");
+  }
+  return data;
+};
+
+export { registerService, logoutService, loginServices, updateProfileService, deleteProfileService };
