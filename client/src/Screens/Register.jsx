8207895bb764257360a27344ba2c553c1fe4f962
vasanth-an <mvasanthanmayilvahanan@gmail.com>
2024-10-17 17:50:44 +0530

    "Updated Navbar and SideBar components, added user authentication and registration functionality with form validation and error handling."

--- client/src/Screens/Register.jsx
+++ client/src/Screens/Register.jsx
@@ -1,43 +1,122 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../Layout/Layout";
 import logo from "../../public/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 import { Input } from "../Components/UsedInputs";
+import { InlineError } from "../Components/Notfications/Error";
+import toast from "react-hot-toast";
+import { registerAction } from "../Redux/Actions/userActions";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { RegisterValidation } from "../Components/Validation/UserValidation";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 
 const Register = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isLoading, isError, userInfo, isSuccess } = useSelector(
+    (state) => state.userRegister
+  );
+
+  //validate user
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(RegisterValidation),
+  });
+  //onSubmit
+  const onSubmit = async (data) => {
+    dispatch(registerAction(data))
+  };
+  //useEffect
+  useEffect(()=>{
+    if(userInfo?.isAdmin){
+      navigate("/dashboard")
+    }
+    else if(userInfo){
+      navigate("/profile")
+    }
+    if(isSuccess){
+     toast.success(`Welcome${userInfo?.fullName}`)
+     dispatch({type:"USER_REGISTER_RESET"})
+    }
+    if(isError){
+      toast.error(isError)
+      dispatch({type:"USER_REGISTER_RESET"})
+    }
+  },[userInfo,isSuccess,isError, navigate, dispatch])
+
   return (
+
     <Layout>
       <div className="container mx-auto px-2 my-24 flex-colo">
-        <dir className="w-full 2xl:w-2/5 gap-8 flex-colo p-8 sm:p-14 md:3/5 bg-dry rounded-lg border border-border">
+      <form
+          onSubmit={handleSubmit(onSubmit)} className="w-full 2xl:w-2/5 gap-8 flex-colo p-8 sm:p-14 md:3/5 bg-dry rounded-lg border border-border">
           <img src={logo} alt="logo" className="w-1/4 h-16 object-cover" />
-          <Input
-            label="Full Name"
-            placeholder="Popcorn Time "
-            type="text"
-            bg={true}
-          />
-          <Input
-            label="Email"
-            placeholder="popcorntime@gmail.com"
-            type="email"
-            bg={true}
-          />
+          <div className="w-full">
+            <Input
+              label="FullName"
+              placeholder="Netflixo React tailwind"
+              type="text"
+              name="fullName"
+              register={register("fullName")}
+            />
+            {errors.fullName && <InlineError text={errors.fullName.message} />}
+          </div>
+      
+          <div className="w-full">
+            <Input
+              label="Email"
+              placeholder="popcorntime@gmail.com"
+              type="email"
+              name="email"
+              register={register("email")}
+              bg={true}
+            />
+            {errors.email && <InlineError text={errors.email.message} />}
+          </div>
+          <div className="w-full">
+            <Input
+              label="password"
+              placeholder="popcorntime@gmail.com"
+              type="password"
+              name="password"
+              register={register("password")}
+              bg={true}
+            />
+            {errors.password && <InlineError text={errors.password.message} />}
+          </div>
           <Input
             label="password"
             placeholder="******"
             type="password"
             bg={true}
           />
-          <Link to="/dashboard" className="bg-subMain transition hover:bg-main flex-rows gap-4 text-white p-4 rounded-lg w-full">
-          <FiLogIn /> Sign Up 
-          </Link>
+          <button
+          type="submit"
+           disabled={isLoading} className="bg-subMain transition hover:bg-main flex-rows gap-4 text-white p-4 rounded-lg w-full">
+          {
+              //if loading show loading
+              isLoading?(
+                "Loading..."
+              ):(
+                <>
+                  <FiLogIn /> Sign In
+                </>
+              )
+            }
+          
+          </button>
           <p className="text-center text-border">
             Already have an account?{" "}
             <Link to="/login" className="text-dryGray font-semibold ml-2">
            Sign In </Link>
           </p>
-        </dir>
+        </form>
       </div>
     </Layout>
   );
