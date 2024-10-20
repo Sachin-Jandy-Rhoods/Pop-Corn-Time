d907fd435c7b14873b5bebf01030ccf897e74ab9
vasanth-an <mvasanthanmayilvahanan@gmail.com>
2024-10-20 14:31:11 +0530

    watch movie

--- client/src/Screens/WatchPage.jsx
+++ client/src/Screens/WatchPage.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../Layout/Layout";
 import { Link, useParams } from "react-router-dom";
-import { Movies } from "../Data/MoviesData";
 import { BiArrowBack } from "react-icons/bi";
 import { FaCloudDownloadAlt, FaHeart, FaPlay } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovieByIdAction } from "../Redux/Actions/MoviesActions";
+import { RiMovie2Line } from "react-icons/ri";
+import Loader from "../Components/Notfications/Loader";
+
 
 const WatchPage = () => {
   let { id } = useParams();
   const dispatch=useDispatch();
   const [play, setPlay] = useState(false);
-  const samClass="w-full gap-6 flex-colo min-h-screen"
+  const sameClass="w-full gap-6 flex-colo min-h-screen"
   //use Selector
   const{isLoading, isError, movie}=useSelector(
     (state)=>state.getMovieById
@@ -25,7 +27,10 @@ useEffect(()=>{
   return (
     <Layout>
       <div className="container mx-auto bg-dry p-6 mb-12">
-        <div className="flex-btn flex-wrap mb-6 gap-2 bg-main rounded border border-gray-800 p-6">
+          {
+            !isError &&
+            <div className="flex-btn flex-wrap mb-6 gap-2 bg-main rounded border border-gray-800 p-6">
+          
           <Link
             to={`/movie/${movie?._id}`}
             className="md:text-xl text-sm flex gap-3 items-center font-bold text-dryGray"
@@ -41,19 +46,39 @@ useEffect(()=>{
             </button>
           </div>
         </div>
+          }
+        
         {/* watch video */}
         {
          play ? (
           <video controls autoPlay={play} className="w-full h-full rounded">
             <source
-              src="../../public/movie.mp4"
+              src={movie?.video}
               type="video/mp4"
-              title={movie.name}
+              title={movie?.name}
             />
           </video>
         ) : (
           <div className="w-full h-screen rounded-lg overflow-hidden relative">
-            <div className="absolute top-0 left-0 bottom-0 right-0 bg-main bg-opacity-30 flex-colo">
+            {
+              isLoading ? (
+                <div className={sameClass}>
+                  <Loader/>
+                </div>
+              ):
+              isError ? (
+                <div className= {sameClass}>
+                <div className="flex-colo w-24 h-24 p-5 mb-4 rounded-full bg-main text-subMain text-4xl">
+                 <RiMovie2Line/>
+ 
+             </div>
+             <p className="text-border text-sm">
+               {isError}
+             </p>
+             </div>
+              ) :(
+                <>
+                 <div className="absolute top-0 left-0 bottom-0 right-0 bg-main bg-opacity-30 flex-colo">
               <button
                 onClick={() => setPlay(true)}
                 className="bg-white text-subMain flex-colo border border-subMain rounded-full w-20 h-20 font-medium text-xl"
@@ -64,12 +89,16 @@ useEffect(()=>{
             <img
               src={
                 movie?.image
-                  ? `../../../public/images/${movie.image}`
+                  ? `../../../public/images/${movie?.image}`
                   : "images/user.png"
               }
               alt={movie?.name}
               className="w-full h-full object-cover rounded-lg"
             />
+                </>
+              )
+            }
+           
           </div>
         )}
       </div>
